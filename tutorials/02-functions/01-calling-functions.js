a1b0f2b4444e51a3a7c167d690f8d214df72ec18
becc-mu <becc.mulugeta@gmail.com>
2019-05-11 01:18:18 +0100

    2 pending

--- tutorials/02-functions/01-calling-functions.js
+++ tutorials/02-functions/01-calling-functions.js
@@ -2,7 +2,11 @@
 function doubleNumber(number){
   return number * 2;
 }
-var timesTwo;
+
+doubleNumber(2);
+ var timesTwo = doubleNumber;
+timesTwo(2);
+console.log(timesTwo.name);
 
 if(typeof window === 'undefined') {
   module.exports = timesTwo;
