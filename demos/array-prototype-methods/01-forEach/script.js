b1a8f72b86633aafa06df978dc78100d23ab5e0b
becc-mu <becc.mulugeta@gmail.com>
2019-05-13 15:37:30 +0100

    iterate over all of the images

--- demos/array-prototype-methods/01-forEach/script.js
+++ demos/array-prototype-methods/01-forEach/script.js
@@ -5,8 +5,14 @@
 
 
 photographs.forEach(function (photograph) {
-  console.log(photograph);
+  //console.log(addInstagramImageToThePage(photograph));
+addInstagramImageToThePage(photograph)
 });
+
 // There is a function defined for you called `addInstagramImageToThePage`,
 // which will take care of adding the image to the page. You just need to worry
 // about iterating over all of the images for this exercise.
+var image = photographs.forEach(function (photograph){
+  photograph = createImageElementFromPhotograph;
+
+});
